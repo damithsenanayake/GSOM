6c1a921f06548d8ba66c0d6f190dece096c31ac1
damithsenanayake <dracusds123@gmail.com>
2018-05-23 14:44:59 +1000

    Better Impl of the Random Sample GSOM

--- GSOM/RandomSampleGSOM.py
+++ GSOM/RandomSampleGSOM.py
@@ -68,7 +68,7 @@ class GSOM(object):
     def smoothen(self, X):
         self.thet_vis_bundle = {}
         r_st = 0.9
-        its = 50
+        its = 10
         lr = self.lr
         print self.wd
         st = timeit.default_timer()
