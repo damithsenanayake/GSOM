fb5a9223602c39a31b560a34c5cc1ee86bd4dfda
damithsenanayake <dracusds123@gmail.com>
2018-05-31 17:44:26 +1000

    Better Impl of the Random Sample GSOM

--- GSOM/mnist_exec.py
+++ GSOM/mnist_exec.py
@@ -26,7 +26,7 @@ print dat.shape
 labels = np.array(fi)[:samples, 0]
 del fi
 gc.collect()
-model = GSOM(lr=.1,  beta=0., sf=0.7, fd = .18, wd=.5)
+model = GSOM(lr=.1,  beta=0., sf=0.7, fd = .18, wd=.055)
 # x, y = MovingMap(iterations=100, beta=1.5).fit_transform(dat[:samples]).T
 Y= model.fit_transform(dat)
 x, y = Y.T
