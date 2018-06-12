8f5a9a847161afcb9de96fb0c9adeed7f225fa93
Damith Senanayake <dracusds123@gmail.com>
2018-06-12 10:26:07 +1000

    FSU YAAAA

--- GSOM/ttester.py
+++ GSOM/ttester.py
@@ -7,6 +7,6 @@ def sigmoid(x):
     return (1./(1+np.exp(-x)))
 
 x = np.array(range(100))
-y =np.exp(-45.5*(x/float(np.amax(x)))**(10))#(1.+(x/100.)**2)**-6#sigmoid(x**2/100.**2)#
+y =np.exp(-4.5*(x/float(np.amax(x)))**(6))#(1.+(x/100.)**2)**-6#sigmoid(x**2/100.**2)#
 plt.plot(x,1-y)
 plt.show()
