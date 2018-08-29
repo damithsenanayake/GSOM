b72777d02bd7e3448febcf3bdb9b7c2834ee0417
Damith Senanayake <dracusds123@gmail.com>
2018-08-29 11:25:45 +1000

    Independent GE function with better consistent results!

--- GSOM/GAPENGSOM.py
+++ GSOM/GAPENGSOM.py
@@ -28,7 +28,7 @@ class GSOM(object):
 
     def train_batch(self, X):
         try:
-            its = 10
+            its = 25
             st = timeit.default_timer()
             self.start_time = st
             self.grid = np.array([[i,j] for i in range(2) for j in range(int(2))])
@@ -43,7 +43,7 @@ class GSOM(object):
 
             lambrad = np.log(rad_min * 1./ self.rst)
             fract_st = 1.
-            min_fract = 0.1
+            min_fract = 0.05
             lrmin = 0.01#*self.lrst#*1./its
             lambda_lr = np.log(lrmin/self.lrst)
 
@@ -56,8 +56,8 @@ class GSOM(object):
                 self.GT = -np.sqrt(X.shape[1]) * np.log(sf) * (X.max() - X.min())
                 self.hits = np.zeros(self.grid.shape[0])
                 r = self.rst*np.exp(lambrad * ntime)# - ntime * (self.rst - rad_min) #(self.rst-rad_min)*(1-ntime) + rad_min#
-                self.wd = 0.02
-                self.lr = self.lrst*(1-ntime)**2#***2#*np.exp(-lambda_lr*ntime)#self.lrst + (min_lr - self.lrst) * ntime**2 #(1-ntime)#
+                self.wd = 0.016
+                self.lr = self.lrst*(1-ntime)#**2#***2#*np.exp(-lambda_lr*ntime)#self.lrst + (min_lr - self.lrst) * ntime**2 #(1-ntime)#
                 xix = 0
                 fract = fract_st*np.exp(-lambda_fr*ntime)
                 self.errors *= 0
@@ -76,7 +76,7 @@ class GSOM(object):
                     theta_d = np.array([thetfunc]).T
                     delta_neis = (x-self.W[neighbors])*theta_d*self.lr
                     ''' Gap  Enforcement '''
-                    wd_coef = self.lr*self.wd#*(fract<0.5)
+                    wd_coef = self.wd*(1-ntime)**0.5#*(fract<0.5)
                     hdist = np.linalg.norm(self.W[decayers]-x, axis=1)
                     hdist /= hdist.max()
                     dist = ldist[decayers]/ldist[decayers].max()
