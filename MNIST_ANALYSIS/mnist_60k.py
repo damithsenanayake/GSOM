9727d5db4a89906bb9e1a0b6929fdf2da1e17183
Damith Senanayake <dracusds123@gmail.com>
2018-12-03 14:19:39 +1100

    Most Consistent Topological Representation of the MNIST 60 K dataset

--- MNIST_ANALYSIS/mnist_60k.py
+++ MNIST_ANALYSIS/mnist_60k.py
@@ -21,7 +21,7 @@ fi = pd.read_csv('~/data/mnist_train.csv', header=None)
 # test = pd.read_csv('../mnist_test.csv', header=None)
 samples = 6000
 
-dat =(np.array(fi)[:samples, 1:])#/255.0
+dat = (np.array(fi)[:samples, 1:])  # /255.0
 dat = PCA(30, random_state=1).fit_transform(dat)
 
 labels = np.array(fi)[:samples, 0]
@@ -31,14 +31,15 @@ print dat.shape
 
 gc.collect()
 st = timeit.default_timer()
-model = GEGSOM(lrst=.5, sf=0.9, fd = .8, radius=6.5, min_rad = 2.,cluster_spacing_factor=1., sd=.02, its=20, labels=labels, momentum=.0, map_structure=6)
+model = GEGSOM(lrst=.2, sf=0.9, fd=.8, radius=8, min_rad=4, cluster_spacing_factor=1., sd=.03, its=25, labels=labels,
+               momentum=.0, map_structure=6)
 
-Y= model.fit_transform(dat)
+Y = model.fit_transform(dat)
 
 et = timeit.default_timer() - st
-hrs = np.floor(et/3600)
-mins = np.floor((et - 3600*hrs)/60)
-secs = et-3600*hrs - 60* mins
+hrs = np.floor(et / 3600)
+mins = np.floor((et - 3600 * hrs) / 60)
+secs = et - 3600 * hrs - 60 * mins
 print 'Total time : ', hrs, ':', mins, ':', secs
 
 x, y = Y.T
@@ -51,12 +52,10 @@ print 'ami :', adjusted_mutual_info_score(labels, kl)
 fig = plt.figure(figsize=(5, 10))
 plt.subplot(211)
 
-np.savetxt('mnist_'+str(samples)+'.csv', np.concatenate((Y, np.array([labels]).T),axis=1))
-plt.scatter(x, y, edgecolors='none',c=plt.cm.jet(labels/10.), alpha = 0.5, s = 15, marker='h')
+np.savetxt('mnist_' + str(samples) + '.csv', np.concatenate((Y, np.array([labels]).T), axis=1))
+plt.scatter(x, y, edgecolors='none', c=plt.cm.jet(labels / 10.), alpha=0.5, s=15, marker='h')
 plt.subplot(212)
 #
-plt.scatter(x, y, edgecolors='none',c=plt.cm.jet(kl/10.), alpha = 0.5, s = 15, marker='h')
-
+plt.scatter(x, y, edgecolors='none', c=plt.cm.jet(kl / 10.), alpha=0.5, s=15, marker='h')
 
 plt.show()
-
