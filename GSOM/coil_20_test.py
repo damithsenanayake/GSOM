2c3743b04733936d5fdbbbe7751841b0bcfba2cb
Damith Senanayake <dracusds123@gmail.com>
2018-12-20 09:03:18 +1100

    Best results so far for COIL 20

--- GSOM/coil_20_test.py
+++ GSOM/coil_20_test.py
@@ -17,10 +17,10 @@ X = fread[:, :-1]
 state = np.random.RandomState(seed=10)
 
 order = state.permutation(np.array(range(X.shape[0])))
-X = PCA(50).fit_transform(X)[order]
+X = PCA(18).fit_transform(X)[order]
 t = fread[:,-1][order]
 imlables = ['duck', 'block 1', 'car 1', 'fs cat', 'Anc', 'Car 2', 'block 2', 'baby powder', 'Tylenol', 'Vaseline', 'Mushroom', 'cup', 'piggy', 'socket', 'pot', 'bottle', 'dish', 'cup 2', 'car 3', 'tub']
-model = GSOM(lrst=1.,sf=0.99999999999999, fd = .9, radius=8, min_rad =5., sd=0.05, cluster_spacing_factor=1.,momentum=0., its=20, labels=t, neighbor_func='gaussian', map_structure=5)#TSNE(perplexity=40)#
+model = GSOM(lrst=1.,sf=0.9999999999999999999999999999999999999999, fd = .9, radius=8., min_rad =8., sd=.0008, cluster_spacing_factor=1.,momentum=0., its=1000, labels=t, neighbor_func='epanechicov', map_structure=6)#TSNE(perplexity=40)#
 Y=model.fit_transform(X)# GSOM().fit_transform(X, lr = 1.0, beta=0.5, sf=0.995, wd=0.02, fd=1.9)
 
 
@@ -29,7 +29,7 @@ colors = [plt.cm.jet(float(i)/max(unique)) for i in unique]
 for i, u in enumerate(unique):
     xi = [Y.T[0][j] for j  in range(len(Y.T[0])) if t[j] == u]
     yi = [Y.T[1][j] for j  in range(len(Y.T[1])) if t[j] == u]
-    plt.scatter(xi, yi, c=colors[i], label=str(imlables[int(u)-1]), alpha=0.7, edgecolors='none')
+    plt.scatter(xi, yi, c=colors[i], label=str(imlables[int(u)-1]), alpha=0.5, edgecolors='none', s = 8)
 # plt.legend()
 
 plt.show()
